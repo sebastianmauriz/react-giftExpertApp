5ab678462847f899b2a5994f8c745f62c911372e
sebastianmauriz@gmail.com <sebastianmauriz@gmail.com>
2021-10-18 19:18:38 -0300

    add docs file

---
+++ docs/precache-manifest.e444fbbdb80ee585e4e847d3b16dc60e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3ff73179d98d9259b6d13b5fce35bfe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0a45b6018525060ebb2",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0a45b6018525060ebb2",
+    "url": "/static/js/main.eb8f8f2a.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
